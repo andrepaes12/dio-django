2cd1e5df63ad229a062b25d1ee444caf4b5c6569
André Paes <aluispaes@gmail.com>
2022-01-13 18:32:04 -0300

    Inclusão das rotas soma, sub, mult e div, ref ao desafio

--- hello_django/urls.py
+++ hello_django/urls.py
@@ -20,4 +20,8 @@ from core import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/<nome>/<int:idade>/', views.hello),
+    path('soma/<int:num1>/<int:num2>/', views.soma),
+    path('sub/<int:num1>/<int:num2>/', views.sub),
+    path('mult/<int:num1>/<int:num2>/', views.mult),
+    path('div/<int:num1>/<int:num2>/', views.div),
 ]
